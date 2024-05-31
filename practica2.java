5271e600732d37ca79ac9f4d453874d36276b077
jul213 <158296107+jul213@users.noreply.github.com>
2024-05-31 20:59:10 +0200

    Update practica2.java

--- practica2.java
+++ practica2.java
@@ -1,4 +1,4 @@
-/* anagramas
+/* anagramas ejercisio 2
  * Escribe una función que reciba dos palabras (String) y retorne
  * verdadero o falso (Bool) según sean o no anagramas.
  * - Un Anagrama consiste en formar una palabra reordenando TODAS
