9eb232c6b7386357f05d1af031ad7cf7b2315ca8
jul213 <158296107+jul213@users.noreply.github.com>
2024-10-26 14:44:56 +0000

    swing

---
+++ swing.java
@@ -0,0 +1,14 @@
+package swing;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JTextPane pricePane;
+    private JTextPane taxPane;
+    private JTextPane totalPane;
+    private JButton buttonCalculate;
+
+    public static void main(string []args) {
+        JFrame frame = new JFrame("swingTax");
+    }
+}
