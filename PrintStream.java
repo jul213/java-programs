d2c8ba667484530f37e01b563322379c9f637322
jul213 <158296107+jul213@users.noreply.github.com>
2024-10-26 12:40:55 +0000

    cerramos el otro fichero

--- PrintStream.java
+++ PrintStream.java
@@ -3,10 +3,11 @@ public class Main {
         try { 
             InputStream in = new FileInputStream("ruta del fichero a leer");
             byte []datos = in.readAllBytes();  //leer fichero
-            in.close() //buena practica cerrar fichero
+            in.close(); //buena practica cerrar fichero
 
             PrintSream out = new PrintStream("destino.txt"); // copiar fichero 
             out.write(datos);
+            out.close(); // cerramos fichero
 
         } catch (Exception e){
             System.out.println("Excepcion" + e.getMessage());
