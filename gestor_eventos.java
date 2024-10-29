913d773020005e7a32a5694198509c53b667e881
jul213 <158296107+jul213@users.noreply.github.com>
2024-10-29 12:28:16 +0000

    creacion de gestor de eventos

---
+++ gestor_eventos.java
@@ -0,0 +1,25 @@
+package swing;
+
+import javax.swing.*;
+import java.util.LinkedList;
+
+interface Mensajero {
+    void saludo();
+}
+
+class Emisor {
+    private LinkedList<Mensajero> hola = new LinkedList<Mensajero>();
+
+    public void receptor(Mensajero recep) {hola.add(recep);}
+
+    public void saludo() {
+        System.out.println("Hola!");
+
+        for (Mensajero i : hola){
+            i.saludo();
+
+        }
+    }
+}
+
+class Receptor implements
