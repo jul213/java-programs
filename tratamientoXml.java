4306e0ba7e209201b89cacf8984575957a366261
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-16 13:21:35 +0000

    primer try

--- tratamientoXml.java
+++ tratamientoXml.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String []args){
         File file = new File(datos.xml);
-        DocumentBuildFactory documentBuilder =  DocumentBuildFactory.newInstance();
+        DocumentBuildFactory documentBuilder =  DocumentBuildFactory.newInstance();ç
+
+        try {
+            DocumentBuilder documentBuilders = documentBuildFactory.newDocumentBuilder();
+
+            try{
+                //analizamos documento
+            }
+        }
     }
 }
