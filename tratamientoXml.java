9ea3c0dc035011d48bb046184b03483851b430f7
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-16 12:36:54 +0000

    primera  parte tratammiento ccon xml

---
+++ tratamientoXml.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String []args){
+        File file = new File(datos.xml);
+        DocumentBuildFactory documentBuilder =  DocumentBuildFactory.newInstance();
+    }
+}
