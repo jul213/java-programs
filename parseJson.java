3a25221a73f3173d6ab964e79597ee4767ed79b6
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-16 23:09:36 +0000

    creacion del inputstream

---
+++ parseJson.java
@@ -0,0 +1,14 @@
+import org.json.*;
+
+public class Main {
+    public static void main(String []args){
+
+        try{
+
+            //creacion del inputstream
+            InputStream is = new FileInputStream("datos.json");
+
+            //
+        }
+    }
+}
