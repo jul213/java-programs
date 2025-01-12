689ed1cb2ba208835be9e1626679a85c7a36bdaf
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-12 20:55:28 +0000

    finalizacion de semaforo.java

--- semaforo/semaforo.java
+++ semaforo/semaforo.java
@@ -1,12 +1,14 @@
 import java.util.Random;
+
 class Semaforo implements Runnable {
     private String verde, rojo, amarillo, color;
     private String[] colores;
+    private Random random;
     public Semaforo() {
         verde = "verde";
         rojo = "rojo";
         amarillo = "amarillo";
-        colores[] = {verde,rojo,amarillo};
+        colores = new String[]{verde,rojo,amarillo};
         Random random = new Random();
     }
     
@@ -17,20 +19,20 @@ class Semaforo implements Runnable {
     public void cambio () {
         if (color.equals("rojo")){
             System.out.println("no pueden pasar");
-            thread.sleep(5000);
+            Thread.sleep(5000);
         }
         else if (color.equals("amarillo")){
             System.out.println("falta poco esperen");
-            thread.sleep(3000);
+            Thread.sleep(3000);
         }
         else {
             System.out.println("pueden pasar");
-            thread.sleep(5500)
+            Thread.sleep(5500)
         }
 
     }
 
-    @override
+    @Override
     public void run(){
         try {
             while (true){
